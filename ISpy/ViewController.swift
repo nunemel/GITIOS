f9ec68cd2827bba77a31aa5217bc85e752d4aad8
Nune Melikyan <nune.melikyan@gmail.com>
2022-08-28 16:50:10 +0400

    Initial Commit

---
+++ ISpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Nune Melikyan on 28.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
